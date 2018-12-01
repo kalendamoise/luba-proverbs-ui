2e75ab6f3e316dc76c65d8f88a9595867e12b0b8
Kalenda M. Kabukala <kalendamoise@gmail.com>
2018-11-30 22:36:52 -0600

    Generated a proverb component, and class. Link them up together and display proverbs on AppComponent.

---
+++ src/app/proverb.ts
@@ -0,0 +1,8 @@
+export class Proverb {
+    id: number;
+    originalText: string;
+    englishText: string;
+    englishMeaning: string;
+    frenchText: string;
+    frenchMeaning: string;
+}
---
+++ src/app/proverbs/proverbs.component.ts
@@ -0,0 +1,26 @@
+import { Component, OnInit } from '@angular/core';
+import { Proverb } from '../proverb';
+
+@Component({
+  selector: 'app-proverbs',
+  templateUrl: './proverbs.component.html',
+  styleUrls: ['./proverbs.component.css']
+})
+export class ProverbsComponent implements OnInit {
+  proverb: Proverb = {
+    id: 1,
+    originalText: 'Kujinga mpala ke bukulu. I musongo wa nzala walwa nao.',
+    englishText: 'Wrinkling the forhead does not make one grown up.',
+    englishMeaning: 'It is wrestling with the pains of hunger. \
+    His forhead is not wrinkled through age, but through hunger, anxiety and the hard knocks of life \
+    Experience counts more than years.',
+    frenchText: '',
+    frenchMeaning: ''
+  };
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
