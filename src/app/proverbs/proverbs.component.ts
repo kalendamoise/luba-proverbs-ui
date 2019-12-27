b53fffc399996a3655e241e5567a51bbf5ea0ba1
Kalenda M. Kabukala <kalendamoise@gmail.com>
2019-12-27 08:47:20 -0500

    Add a list of categories and update some css.

--- src/app/proverbs/proverbs.component.ts
+++ src/app/proverbs/proverbs.component.ts
@@ -1,24 +1,43 @@
-import { Component, OnInit } from '@angular/core';
-import { Proverb } from '../proverb';
-import { ProverbService } from '../proverb.service';
+import { Component, OnInit, AfterViewInit } from "@angular/core";
+import { Proverb } from "../proverb";
+import { ProverbService } from "../proverb.service";
+import {CATEGORIES } from "../../category_list";
 
 @Component({
-  selector: 'app-proverbs',
-  templateUrl: './proverbs.component.html',
-  styleUrls: ['./proverbs.component.css']
+  selector: "app-proverbs",
+  templateUrl: "./proverbs.component.html",
+  styleUrls: ["./proverbs.component.css"]
 })
 export class ProverbsComponent implements OnInit {
-
-  proverbs: Proverb[];
+  proverbs: Proverb[] =  [];
   selectedCategory = 'All';
+  categories = CATEGORIES;
+  categories_dict: { [id: number]: string; } = {};
 
-  constructor(private proverbService: ProverbService) { }
+  constructor(private proverbService: ProverbService) {
+    this.categories.forEach((item , index) => {
+      this.categories_dict[index] = item;
+    });
+  }
 
   ngOnInit() {
     this.getProverbs();
   }
 
+  addCategoris(): void {
+    this.proverbs.forEach(item => {
+      item.quategory.push(Math.floor(Math.random() * this.proverbs.length));
+      item.quategory.push(Math.floor(Math.random() * this.proverbs.length));
+      console.log('Item is: ' + JSON.stringify(item));
+    });
+  }
+
   getProverbs(): void {
-    this.proverbService.getProverbs().subscribe(proverbs => this.proverbs = proverbs);
+    this.proverbService
+      .getProverbs()
+      .subscribe(items => {
+        this.proverbs = items;
+        this.addCategoris();
+      });
   }
 }
